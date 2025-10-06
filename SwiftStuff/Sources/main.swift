c421bef23dbebba3034e2d7a72bb39cefd65954a
Gen-0sis <jerryhsieh0329@gmail.com>
2025-10-06 12:48:35 -0500

    Add files via upload

--- SwiftStuff/Sources/main.swift
+++ SwiftStuff/Sources/main.swift
@@ -60,8 +60,11 @@ public class Player {
     //hand is an empty array of card objects
     var hand: [Card] = [Card]()
     var balance: Int = 500
-    var ready: Bool = false;
+    var ready: Bool = false
     var name: String = "Unnamed"
+        var splitted: Bool = false
+        var hand2: [Card] = [Card]()//hand only used if user has split
+    
 
     init(name Name: String) {
         self.name = Name
@@ -79,17 +82,22 @@ public class Player {
     public func hit(deck deckToUse: Deck) {
         hand.append(deckToUse.removeCard(index: 0))
     }
-    public func stand(bet Money: Int) {
+    public func stand() {
         ready = true
-        balance -= Money
         print("\(name) has decided to stand")
     }
-    public func doubleDown(deck deckToUse: Deck, bet Money: Int) {
+    public func doubleDown(deck deckToUse: Deck) {
         hand.append(deckToUse.removeCard(index: 0))
-        stand(bet: Money)//an issue here
+        stand()//an issue here
     }
-    public func split() {
+    public func split() { //you can't split multiple times in a row
         
+    }
+    public func declare() -> Int{
+        return 0
+    }
+    public func question() {
+
     }
     
 
@@ -104,7 +112,7 @@ public class Manager { //and all the empty husks cried out one word...
 
     public func raisePot(money amount: Int){
         pot += amount * numPlayers
-        //respective amount needs to be subtracted from all players
+        //respective amount needs to be subtracted from all players by iterating through the player list, do not mind if someone is negative yet
 
     }
     
@@ -114,7 +122,13 @@ public class Manager { //and all the empty husks cried out one word...
     public func endRound() {//a measure of player values (find the maximum value, but may need to see if there is a tie), then who gets how much money
 
     }
-    public func BLACKJACK() -> Void{
+    public func clear(){
+        for _ in 1...20{
+            print()
+        }
+        
+    }
+    public func BLACKJACK(){
       print("BLACKKKKKKKKKKJACKKKKKKKKKKKKKKKKKK!!!!!!!!!!!!!!!") 
     }
     public func checkName(name Name: String) -> String {
@@ -141,27 +155,62 @@ public class Manager { //and all the empty husks cried out one word...
 //the gameplay loop
 
 //amount of players needed
-        print("how many players are you going to be playing with")
-        numPlayers = Int(readLine()!)! //ill need to check if this is larger than 0 eventually...
+
+
+//NEED TO FIX
+        /*print("how many players are you going to be playing with (Minimum of 2 and Maximum of 7)")
+        while numPlayers <= 1{
+          do {
+            if (numPlayers <= 7){
+                numPlayers = try Int(readLine()!)!
+            }
+            else{
+                print("you have inputted above 7 players, please try again")
+            }
+          }
+          catch{
+            print("Invalid input of number of players, make sure it is an integer")
+          }  
+        }*/
+        
+
+
         print("You have chosen to play with \(numPlayers) real players!") 
         generatePlayers()
     var done = false
 //the loop begins
     while !done {
     //ask every player for their bet, take the highest bet and make everyone add it to the pot (without anyone going bankrupt, if someone is going to go bankrupt, turn them negative)
-
+                            //this can be done within this function
+        var maxBet: Int = Int.min
+        for i in 0...numPlayers-1 {
+            
+        }
         //deal out the cards
-
-        //start with the end of the array
-
+                            //also can be done within this functoin
+        //start with the end of the array and incrementing towards the dealer
+        for i in stride(from: numPlayers-1, to: -1, by: -1) {
+  
         //ask about what their action is after showing them their cards (raise pot if necessary)
-        //decrease the index of the array
+                            //done within a function of the player class
+            players[i].question()
         //send a bunch of new lines to clear screen so the next player isn't spoiled
-
-    //repeat previous 3 steps (until got the actions of all the players)
-
+            clear()
+        }
     //display the cards of everyone
+        var largest: Int = Int.min
+        for i in 0...numPlayers-1{
+            if largest < players[i].declare() {
+
+            }
+            else if largest == players[i].declare(){
+                //force a pushback
+                break; //this should only break out of the for loop
+            }
+             //will need to get the index of when max makes largest < players[i].declare but also if the largest equal to one of those, then need to force a pushback
+        }
     //whoever wins gets all the money
+
     //if anyone is negative money they are removed from the game (make sure not to take bankrupt money as real money, and only take the balance of the player before they went bankrupt)
 
     //repeat asking for bets until only one player remains
